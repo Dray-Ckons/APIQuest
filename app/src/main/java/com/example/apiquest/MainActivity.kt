485535b017bf1dc9fae55e47ba96c81f785ed5cd
Dray-Ckons <drayckons14@gmail.com>
2023-12-07 09:18:04 -0300

    Initial commit

---
+++ app/src/main/java/com/example/apiquest/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.apiquest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        
+
+    }
+
+
+    fun execute(urlstring: String){
+        GlobalScope.launch (Dispatchers.Main){
+            val result = withContext(Dispatchers.10){
+                performApiRequest(urlstring)
+            }
+        }
+    }
+
+}
